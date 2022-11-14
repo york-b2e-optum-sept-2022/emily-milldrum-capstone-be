a8f0861c31524ac347b9cfabcbbf3764c4b86b81
Emily <emilldr>
2022-11-14 15:54:58 -0600

    init

---
+++ src/main/java/net/yorksolutions/emilymilldrumcapstonebe/EmilyMilldrumCapstoneBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.emilymilldrumcapstonebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmilyMilldrumCapstoneBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmilyMilldrumCapstoneBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/emilymilldrumcapstonebe/EmilyMilldrumCapstoneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.emilymilldrumcapstonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmilyMilldrumCapstoneBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
